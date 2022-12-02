80ba8a63f91074df3d5ac141149ab49636337453
RedSigma07 <julianstipovic@gmx.de>
2022-12-02 16:15:33 +0100

    Update App.vue

--- frontend-vue/src/App.vue
+++ frontend-vue/src/App.vue
@@ -1,3 +1,28 @@
 <template>
-  <h1>TEST</h1>
+  <h1>This is a simple {{name}} - Test age: {{age}}</h1>
 </template>
+
+<script setup>
+  import {ref} from 'vue'
+
+  const name = ref ('Test')
+  const age = ref ('21')
+</script>
+
+<!--
+<script>
+import {ref} from 'vue'
+
+export default {
+  setup() {
+    const name = ref ('Test')
+    const age = ref ('21')
+
+    return {
+      name,
+      age
+    }
+  }
+}
+</script>
+-->
