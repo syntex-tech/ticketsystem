7978b90e5126657bcbda9534a0d415e09873ee62
RedSigma07 <julianstipovic@gmx.de>
2022-12-08 14:10:55 +0100

    cleanup

--- frontend-vue/src/App.vue
+++ frontend-vue/src/App.vue
@@ -1,28 +1,7 @@
 <template>
-  <h1>This is a simple {{name}} - Test age: {{age}}</h1>
+  
 </template>
 
 <script setup>
-  import {ref} from 'vue'
-
-  const name = ref ('Test')
-  const age = ref ('21')
+  
 </script>
-
-<!--
-<script>
-import {ref} from 'vue'
-
-export default {
-  setup() {
-    const name = ref ('Test')
-    const age = ref ('21')
-
-    return {
-      name,
-      age
-    }
-  }
-}
-</script>
--->
