457a2fa4e37979e91de7bd3062a2feeee63f5f06
RedSigma07 <julianstipovic@gmx.de>
2022-12-09 16:00:03 +0100

    example pictures added

--- frontend-vue/src/components/Content.vue
+++ frontend-vue/src/components/Content.vue
@@ -12,7 +12,7 @@
               <img class="first-slide" src="../assets/images/panorama_mountains3.jpg" alt="First slide">
               <div class="container">
                 <div class="carousel-caption text-border">
-                  <h1>SkyPath.</h1>
+                  <h1>Savor Liberty.</h1>
                   <p>We offer you an easy path to peace, freedom and happiness - the mountains. Start your journey now.</p>
                   <p><a class="btn btn-lg btn-primary btn-opac" href="#" role="button">Book your tickets now</a></p>
                 </div>
@@ -59,21 +59,21 @@
           <!-- Three columns of text below the carousel -->
           <div class="row">
             <div class="col-lg-4">
-              <img class="rounded-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
-              <h2>Heading</h2>
-              <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
+              <img class="rounded-circle border border-info" src="../assets/images/small icons/solo.png" alt="Generic placeholder image" width="140" height="140">
+              <h2>SkiPass SOLO</h2>
+              <p>We need to add a meaningful text here.. Not sure what it is gonna be.</p>
               <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
             </div><!-- /.col-lg-4 -->
             <div class="col-lg-4">
-              <img class="rounded-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
-              <h2>Heading</h2>
-              <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p>
+              <img class="rounded-circle border border-info" src="../assets/images/small icons/combo.png" alt="Generic placeholder image" width="140" height="140">
+              <h2>SkiPass COMBO</h2>
+              <p>We need to add a meaningful text here.. Not sure what it is gonna be.</p>
               <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
             </div><!-- /.col-lg-4 -->
             <div class="col-lg-4">
-              <img class="rounded-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
-              <h2>Heading</h2>
-              <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+              <img class="rounded-circle border border-info" src="../assets/images/small icons/family.png" alt="Generic placeholder image" width="140" height="140">
+              <h2>SkiPass FAMILY</h2>
+              <p>We need to add a meaningful text here.. Not sure what it is gonna be.</p>
               <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
             </div><!-- /.col-lg-4 -->
           </div><!-- /.row -->
--- frontend-vue/src/components/Navbar.vue
+++ frontend-vue/src/components/Navbar.vue
@@ -1,6 +1,6 @@
 <template>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <a class="navbar-brand" href="#">Navbar</a>
+  <a class="navbar-brand" href="#">SkyPath</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -28,6 +28,7 @@
         <a class="nav-link disabled" href="#">Disabled</a>
       </li>
     </ul>
+    
     <form class="form-inline my-2 my-lg-0">
       <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
       <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
@@ -35,3 +36,10 @@
   </div>
 </nav>
 </template>
+
+<style>
+  
+  
+
+
+</style>
