a9eb63d5f26812bc3d4062049886309b8f551e1d
FabiQarqur <91314373+FabiQarqur@users.noreply.github.com>
2022-12-14 18:16:22 +0100

    Create validation.js

---
+++ backend-nodejs/validation.js
@@ -0,0 +1,41 @@
+//VALIDATION
+const Joi = require('@hapi/joi');
+
+//register Validation
+const registerValidation = (data) =>{
+
+    const schema = Joi.object({
+        name: Joi.string()
+        .min(6)
+        .required(),
+        email: Joi.string()
+        .min(6)
+        .required()
+        .email(),
+        password: Joi.string()
+        .min(6)
+        .required()
+    });
+    return schema.validate(data);
+}
+
+const loginValidation = (data) =>{
+
+    const schema = Joi.object({
+        name: Joi.string()
+        .min(6)
+        .required(),
+        email: Joi.string()
+        .min(6)
+        .required()
+        .email(),
+        password: Joi.string()
+        .min(6)
+        .required()
+    });
+    return schema.validate(data);
+}
+
+
+module.exports.registerValidation = registerValidation;
+module.exports.loginValidation = loginValidation;
