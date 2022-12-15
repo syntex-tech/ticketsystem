0bbd3e2e25021663cebd38ff545c53df874b38eb
FabiQarqur <91314373+FabiQarqur@users.noreply.github.com>
2022-12-15 18:08:21 +0100

    ConnecttoDBFabi15.12

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const mongoose = require('mongoose');
 const index = express();
 
 //ROUTES
@@ -7,5 +7,11 @@ const index = express();
 index.get('/', (req,res) => {
     res.send('We are on home');
 });
+
+//Connect to DB
+
+mongoose.set('strictQuery', true);
+mongoose.connect('mongodb+srv://Oberwallis:admin@cluster0np.emsr4vh.mongodb.net/?retryWrites=true&w=majority', () => console.log('Connected to DB!'));
+
 
 index.listen(3000);
