017ae347d6b60573ca6e1ba7a1c0bf7d04bd02dc
FabiQarqur <91314373+FabiQarqur@users.noreply.github.com>
2023-01-16 10:21:12 +0100

    app.use rausnehmen aus ticket

--- backend-nodejs/routes/ticket.js
+++ backend-nodejs/routes/ticket.js
@@ -1,10 +1,5 @@
 const express = require('express');
 const app = express();
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "http://localhost:5173");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
 const router = express.Router();
 const SkipassTicket = require('../model/SkipassTicket');
 
