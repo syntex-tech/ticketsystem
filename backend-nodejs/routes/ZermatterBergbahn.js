b1f3ac50025c79e64310fb35fe6dd2cfd7957436
FabiQarqur <91314373+FabiQarqur@users.noreply.github.com>
2023-01-22 04:12:50 +0100

    update2 zermatterbergbahn

--- backend-nodejs/routes/ZermatterBergbahn.js
+++ backend-nodejs/routes/ZermatterBergbahn.js
@@ -12,11 +12,11 @@ router.get('/', (req,res) => {
 
 router.post('/', async (req, res) => {
     res.contentType('application/json');
-    res.type('json');
-      
+    res.type('json');  
+    
     //Checking if the id exist
     const AnzahlZutritteZermatterBergbahnen = await SkipassTicket.findOne({
-        id: req.body.ids,
+        _id: req.body.ids,
     });
     if(!SkipassTicket) {
         res.status(400);
@@ -24,25 +24,22 @@ router.post('/', async (req, res) => {
             message: 'Ticket not found'
         });
     }
-
+    //if statement muss noch gemacht werden Die Zahl 5 wird noch nicht erkannt und das System denkt, dass es kleiner ist als 0
+    if(AnzahlZutritteZermatterBergbahnen > 0) {
     const AnzahlZutritteZermatterBergbahnen2 = await SkipassTicket.findOneAndUpdate({
-        id: req.body.ids,
+        _id: req.body.ids,
     },
     {
-        AnzahlZutritteZermatterBergbahnen: req.body.id
+        $inc: {AnzahlZutritteZermatterBergbahnen: -1}
     });
-
-    res.send({
-        message: 'Ticket aktualisiert'
-    });
+    return res.send({
+        message: 'Die Anzahl der Zutritte wurde aktualisiert!'
+    })
+}else {
+    return res.send({
+        message: "Sie haben bereits alle 5 Zutritte der Bergbahn verbraucht!"
+    })
+  }
 });
-
-
-
-
-
-
-
-
 
 module.exports = router;
