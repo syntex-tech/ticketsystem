9f478b875fe58f523cecc0dea0dcb07c1cf58a90
FabiQarqur <91314373+FabiQarqur@users.noreply.github.com>
2023-01-22 20:52:58 +0100

    fixedemail

--- backend-nodejs/routes/ticket.js
+++ backend-nodejs/routes/ticket.js
@@ -10,7 +10,7 @@ router.get('/', (req,res) => {
 });
 
 //Erstellung des SkipassTicket
-router.post('/', async (req,res) => {
+/*router.post('/', async (req,res) => {
     
     const skipassticket = new SkipassTicket({
         Email: req.body.Email,
@@ -25,20 +25,57 @@ router.post('/', async (req,res) => {
     try{
         const savedSkipassTicket = await skipassticket.save();
         res.json(savedSkipassTicket);
+        sendEmail()
+        .then(response => res.send(response.message))
+        .catch(error => res.status(500).send(error.message))
     } catch(err){
         res.json({message: err });
-    }
+    }*/
 
 
 
-    sendEmail()
+    /*sendEmail()
     .then(response => res.send(response.message))
-    .catch(error => res.status(500).send(error.message))
+    .catch(error => res.status(500).send(error.message))*/
 
+    router.post('/', async (req,res) => {
+        let responseSent = false;
+        const skipassticket = new SkipassTicket({
+            Email: req.body.Email,
+            Vorname: req.body.Vorname,
+            Nachname: req.body.Nachname,
+            Stadt: req.body.Stadt,
+            Postleitzahl: req.body.Postleitzahl,
+            Strasse: req.body.Strasse,
+            Hausnummer: req.body.Hausnummer,
+            //id: mongoose.Types.ObjectId
+        });
+        try{
+            const savedSkipassTicket = await skipassticket.save();
+            res.json(savedSkipassTicket);
+            responseSent = true;
+            sendEmail()
+            .then(response => {
+                if (!responseSent) {
+                    res.send(response.message)
+                    responseSent = true;
+                }
+            })
+            .catch(error => {
+                if (!responseSent) {
+                    res.status(500).send(error.message)
+                    responseSent = true;
+                }
+            })
+        } catch(err){
+            if (!responseSent) {
+                res.json({message: err });
+                responseSent = true;
+            }
+        }
     
-});
 
-function sendEmail(){
+    function sendEmail(){
 
     return new Promise((resolve, reject) => {
         var transporter = nodemailer.createTransport({
@@ -56,7 +93,7 @@ function sendEmail(){
         text: "Du hast den Skipass Oberwallis gekauft! Folgende Daten werden gespeichert. " + 
         "Email: " + skipassticket.Email +  " Name: " + skipassticket.Vorname + " " 
         + skipassticket.Nachname + " Straße: " + skipassticket.Strasse + " " + skipassticket.Hausnummer 
-        + " Stadt: " + skipassticket.Postleitzahl + " " + skipassticket.Stadt + " Ticketnummer: " + skipassticket.id,
+        + " Stadt: " + skipassticket.Postleitzahl + " " + skipassticket.Stadt + " Ticketnummer: " + skipassticket._id,
                        
     }
     transporter.sendMail(mail_configs, function(error, info){
@@ -68,6 +105,10 @@ function sendEmail(){
     })
     })
   }
+});
+
+
+
 
 //Ticket suchen
 
