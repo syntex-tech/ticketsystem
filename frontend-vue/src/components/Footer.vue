4dc1cfff847552b60e230549e591ef2d0b9d3dba
TurckMalte <83653263+TurckMalte@users.noreply.github.com>
2023-02-11 18:47:54 +0100

    Update Footer.vue

--- frontend-vue/src/components/Footer.vue
+++ frontend-vue/src/components/Footer.vue
@@ -7,41 +7,6 @@
                 class="text-center text-lg-start text-white"
                 style="background-color: #1c2331"
                 >
-        <!-- Section: Social media -->
-        <section
-                class="d-flex justify-content-between p-4"
-                style="background-color: rgba(0, 0, 0, 0.2)"
-                >
-            <!-- Left -->
-            <div class="me-5">
-            <span>Get connected with us on social networks:</span>
-            </div>
-            <!-- Left -->
-    
-            <!-- Right -->
-            <div>
-            <a href="https://www.facebook.com/dhbwmosbach.home/" class="text-white me-4">
-                <i class="fab fa-facebook-f"></i>
-            </a>
-            <a href="" class="text-white me-4">
-                <i class="fab fa-twitter"></i>
-            </a>
-            <a href="" class="text-white me-4">
-                <i class="fab fa-google"></i>
-            </a>
-            <a href="" class="text-white me-4">
-                <i class="fab fa-instagram"></i>
-            </a>
-            <a href="" class="text-white me-4">
-                <i class="fab fa-linkedin"></i>
-            </a>
-            <a href="" class="text-white me-4">
-                <i class="fab fa-github"></i>
-            </a>
-            </div>
-            <!-- Right -->
-        </section>
-        <!-- Section: Social media -->
     
         <!-- Section: Links  -->
         <section class="">
@@ -51,74 +16,29 @@
                 <!-- Grid column -->
                 <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
                 <!-- Content -->
-                <h6 class="text-uppercase fw-bold">SkyPath</h6>
+                <p></p>
+                <h6 class="text-uppercase fw-bold">Oberwallis</h6>
                 <hr
                     class="mb-4 mt-0 d-inline-block mx-auto"
-                    style="width: 60px; background-color: lightblue; height: 2px"
+                    style="width: 60px; background-color: rgb(57, 131, 177); height: 2px"
                     />
                 <p>
-                    Start your journey now with SkyPath!
+                    Start your journey now with Oberwallis!
                 </p>
                 </div>
                 <!-- Grid column -->
-    
-                <!-- Grid column -->
-                <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
-                <!-- Links -->
-                <h6 class="text-uppercase fw-bold">Test123</h6>
-                <hr
-                    class="mb-4 mt-0 d-inline-block mx-auto"
-                    style="width: 60px; background-color: lightblue; height: 2px"
-                    />
-                <p>
-                    <a href="#!" class="text-white">Ticketshop</a>
-                </p>
-                <p>
-                    <a href="#!" class="text-white">Test2</a>
-                </p>
-                <p>
-                    <a href="#!" class="text-white">Test3</a>
-                </p>
-                <p>
-                    <a href="#!" class="text-white">Test4</a>
-                </p>
-                </div>
-                <!-- Grid column -->
-    
-                <!-- Grid column -->
-                <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
-                <!-- Links -->
-                <h6 class="text-uppercase fw-bold">Test123</h6>
-                <hr
-                    class="mb-4 mt-0 d-inline-block mx-auto"
-                    style="width: 60px; background-color: lightblue; height: 2px"
-                    />
-                <p>
-                    <a href="#!" class="text-white">Your Account</a>
-                </p>
-                <p>
-                    <a href="#!" class="text-white">Info</a>
-                </p>
-                <p>
-                    <a href="#!" class="text-white">Season Info</a>
-                </p>
-                <p>
-                    <a href="#!" class="text-white">Delete my account</a>
-                </p>
-                </div>
-                <!-- Grid column -->
-    
                 <!-- Grid column -->
                 <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
                 <!-- Links -->
-                <h6 class="text-uppercase fw-bold">Contact</h6>
+                <p></p>
+                <h6 class="text-uppercase fw-bold">Kontakt</h6>
                 <hr
                     class="mb-4 mt-0 d-inline-block mx-auto"
-                    style="width: 60px; background-color: lightblue; height: 2px"
+                    style="width: 60px; background-color: rgb(57, 131, 177); height: 2px"
                     />
-                <p><i class="fas fa-home mr-3"></i> DHBW Mosbach </p>
-                <p><i class="fas fa-envelope mr-3"></i> info@AlpTicket.de</p>
-                <p><i class="fas fa-phone mr-3"></i> 07131 1234567</p>
+                <p><i class="fas fa-home mr-3"></i>Lohrtalweg 10, 74821 Mosbach</p>
+                <p><i class="fas fa-envelope mr-3"></i> SkipassOberwallis@web.de</p>
+                <p><i class="fas fa-phone mr-3"></i> +49 152 1234567</p>
                 
                 </div>
                 <!-- Grid column -->
@@ -133,9 +53,9 @@
             class="text-center p-3"
             style="background-color: rgba(0, 0, 0, 0.2)"
             >
-            © 2020 Copyright:
+            © 2023 Copyright:
             <a class="text-white" href="#"
-            >SkyPath</a
+            >Oberwallis</a
             >
         </div>
         <!-- Copyright -->
