7e8e527cf542a6a2599c99c399432aac806fbc52
RedSigma07 <julianstipovic@gmx.de>
2023-02-11 13:10:11 +0100

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ require('dotenv/config');
 
 
 index.use(cors({
-    origin: ["http://localhost:5173", "127.0.0.1:5173"],
+    origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
     credentials: true
 }));
 
